34a4416e02a0f413592a77c0fbe9f4dae44db013
Davide Carita <davide.carita@alten.it>
2024-04-29 13:00:48 +0200

    initial commit

---
+++ src/app/components/comune-detail/comune-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comune-detail',
+  templateUrl: './comune-detail.component.html',
+  styleUrl: './comune-detail.component.scss'
+})
+export class ComuneDetailComponent {
+
+}
